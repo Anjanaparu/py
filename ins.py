103e71a04a9484024276f9996f5e255adf29ce93
Anjanaparu <krishnaanjana253@gmail.com>
2025-09-19 10:34:53 +0100

    commit

---
+++ ins.py
@@ -0,0 +1,5 @@
+my_list=[1,2,3,                                                                                                                                                                                                                                                                                                                                                                                        5]
+obj=4
+position=3
+my_list.insert(position -1,obj)
+print("updated list:",my_list)
